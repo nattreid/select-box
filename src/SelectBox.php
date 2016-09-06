710a53faa5b8daccd05ff3c8193d1a2b2a7fe4a0
attreid <attreid@gmail.com>
2016-09-06 10:44:29 +0200

    odsazeni

--- src/SelectBox.php
+++ src/SelectBox.php
@@ -7,67 +7,73 @@ namespace NAttreid\SelectBox;
  *
  * @author Attreid <attreid@gmail.com>
  */
-class SelectBox extends \Nette\Application\UI\Control {
+class SelectBox extends \Nette\Application\UI\Control
+{
 
-    private $args = [];
+	private $args = [];
 
-    /**
-     * Nastavi sloupec ve formulari
-     * @param string $column
-     */
-    public function setColumn($column) {
-        $this->args['column'] = $column;
-    }
+	/**
+	 * Nastavi sloupec ve formulari
+	 * @param string $column
+	 */
+	public function setColumn($column)
+	{
+		$this->args['column'] = $column;
+	}
 
-    /**
-     * Nastavi nazev
-     * @param string $name
-     */
-    public function setName($name) {
-        $this->args['name'] = $name;
-    }
+	/**
+	 * Nastavi nazev
+	 * @param string $name
+	 */
+	public function setName($name)
+	{
+		$this->args['name'] = $name;
+	}
 
-    /**
-     * Nastavi data do selectu
-     * @param array $rows
-     */
-    public function setRows($rows) {
-        $this->args['rows'] = $rows;
-    }
+	/**
+	 * Nastavi data do selectu
+	 * @param array $rows
+	 */
+	public function setRows($rows)
+	{
+		$this->args['rows'] = $rows;
+	}
 
-    /**
-     * Nastavi prvni radek
-     * @param string $first
-     */
-    public function setFirst($first) {
-        $this->args['first'] = $first;
-    }
+	/**
+	 * Nastavi prvni radek
+	 * @param string $first
+	 */
+	public function setFirst($first)
+	{
+		$this->args['first'] = $first;
+	}
 
-    public function render($args = NULL) {
-        $template = $this->template;
-        $template->setFile(__DIR__ . '/selectBox.latte');
+	public function render($args = NULL)
+	{
+		$template = $this->template;
+		$template->setFile(__DIR__ . '/selectBox.latte');
 
-        foreach ($this->args as $name => $value) {
-            $template->$name = $value;
-        }
-        if ($args !== NULL) {
-            foreach ($args as $name => $value) {
-                $template->$name = $value;
-            }
-        }
-        
-        $rows = $template->rows;
-        if ($rows instanceof \Countable) {
-            $template->count = $rows->count();
-        } elseif ($rows instanceof \Traversable) {
-            $template->count = iterator_count($rows);
-        } elseif (is_array($rows)) {
-            $template->count = count($rows);
-        } else {
-            throw new \InvalidArgumentException('Rows neni pocitatelne');
-        }
+		foreach ($this->args as $name => $value) {
+			$template->$name = $value;
+		}
+		if ($args !== NULL) {
+			foreach ($args as $name => $value) {
+				$template->$name = $value;
+			}
+		}
 
-        $template->render();
-    }
+		$rows = $template->rows;
+		if ($rows instanceof \Countable) {
+			$template->count = $rows->count();
+		} elseif ($rows instanceof \Traversable) {
+			$template->count = iterator_count($rows);
+		} elseif (is_array($rows)) {
+			$template->count = count($rows);
+		} else {
+			throw new \InvalidArgumentException('Rows neni pocitatelne');
+		}
+
+		$template->render();
+	}
 
 }
